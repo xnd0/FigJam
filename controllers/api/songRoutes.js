8be07b833b1ef30dd76281b161b2061db7fd779b
xndcmd <xndcmd@gmail.com>
2022-07-11 13:20:44 -0700

    successfully gets a response in postman

--- controllers/api/songRoutes.js
+++ controllers/api/songRoutes.js
@@ -4,14 +4,14 @@ const Song = require('../../models/song.js');
 //GET all songs (not sure about my syntax here, please make corrections if needed)
 router.get('/', (req, res) => {
     Song.findAll().then((songData) => {
-        res.json(bookData);
+        res.json(songData);
     })
 });
 
 //GET a single song
 router.get('/:id', (req, res) => {
     Song.findByPk(req.params.id).then((songData) =>{
-        res.json(bookData)
+        res.json(songData)
     });
 });
 
