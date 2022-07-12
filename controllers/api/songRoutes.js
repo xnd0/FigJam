93d79b55db92f3e04423a1f67b360ef63cf533f2
xndcmd <xndcmd@gmail.com>
2022-07-12 07:33:56 -0700

    setup songRoutes.js for initial POST request

--- controllers/api/songRoutes.js
+++ controllers/api/songRoutes.js
@@ -1,29 +1,34 @@
+// ------------------------- //
+// ----- songRoutes.js ----- //
+// ------------------------- //
+
+
 const router = require('express').Router()
 const Song = require('../../models/song.js');
 
 
 
+// CREATE new post
+router.post('/', async (req, res) => {
+    try {
+      const dbUserData = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
+  
+      // Set up sessions with a 'loggedIn' variable set to `true`
+      req.session.save(() => {
+        req.session.loggedIn = true;
+  
+        res.status(200).json(dbUserData);
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
-
-
-
-
-
-
-
-//GET all songs (not sure about my syntax here, please make corrections if needed)
-// router.get('/', (req, res) => {
-//     Song.findAll().then((songData) => {
-//         res.json(songData);
-//     })
-// });
-
-// //GET a single song
-// router.get('/:id', (req, res) => {
-//     Song.findByPk(req.params.id).then((songData) =>{
-//         res.json(songData)
-//     });
-// });
 
 module.exports = router;
     
