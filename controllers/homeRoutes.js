40b81a14d1e8f28365d61354e7827c5a8413ac00
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-07-12 13:13:19 -0700

    little bit of css and organizing files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -42,7 +42,7 @@ router.get('/add-song', async (req, res) => {
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/profile');
+      res.redirect('/');
       return;
     }
   
