b267e339d6928c3d9cacd3ffacb92b7ba065955d
xndcmd <xndcmd@gmail.com>
2022-07-12 07:38:53 -0700

    adjusted variable names in songRoutes.js to reflect model Song.js

--- controllers/api/songRoutes.js
+++ controllers/api/songRoutes.js
@@ -4,25 +4,25 @@
 
 
 const router = require('express').Router()
-const Song = require('../../models/song.js');
+const { Song } = require('../../models');
 
 
 
 // CREATE new post
 router.post('/', async (req, res) => {
     try {
-      const dbUserData = await User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
+      const dbSongData = await Song.create({
+        song_name: req.body.song_name,
+        artist_name: req.body.artist_name,
+        // password: req.body.password,
       });
   
       // Set up sessions with a 'loggedIn' variable set to `true`
-      req.session.save(() => {
-        req.session.loggedIn = true;
+    //   req.session.save(() => {
+    //     req.session.loggedIn = true;
   
-        res.status(200).json(dbUserData);
-      });
+    //     res.status(200).json(dbSongData);
+    //   });
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
