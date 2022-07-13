5a3150e7125857a2bec47c386db6653f38ae592c
xndcmd <xndcmd@gmail.com>
2022-07-13 11:06:51 -0700

    working on adding url + comment, form, and route functionality

--- public/js/post.js
+++ public/js/post.js
@@ -1,14 +1,29 @@
+
+
+
+
+
+
 const post = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
+
+  //make these good
+  const song_name = document.querySelector('#song_name').value.trim();
+  const artist_name = document.querySelector('#artist_name').value.trim();
+  const comment_text = document.querySelector('#comment_text').value.trim();
+
+  const response = await fetch(`/api/songs`, {
+    method: 'POST',
+    body: JSON.stringify({ song_name, artist_name, url, comment_text }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/songs');
+  } else {
+    alert('Failed to create project');
+  }
   };
   
   document.querySelector('#post').addEventListener('click', logout);
