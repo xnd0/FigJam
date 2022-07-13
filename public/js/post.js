83f07038016069142598e9fb604a7b1f7c29de7e
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-07-13 10:04:27 -0700

    added date and styled a little more

---
+++ public/js/post.js
@@ -0,0 +1,15 @@
+const post = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#post').addEventListener('click', logout);
+  
