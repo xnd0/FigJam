0f0a2c20bfea744b59fbd309d172a4f4583244eb
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-07-13 11:05:25 -0700

    cloudinary upload photo working

--- public/js/post.js
+++ public/js/post.js
@@ -1,15 +1,35 @@
-const post = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
+let url;
+const post = async (event) => {
+  event.preventDefault();
+  // console.log(url);
+    // const response = await fetch('/api/users/logout', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    // });
   
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
+    // if (response.ok) {
+    //   document.location.replace('/');
+    // } else {
+    //   alert(response.statusText);
+    // }
+
   };
-  
-  document.querySelector('#post').addEventListener('click', logout);
-  
+  var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'deubd4mph', 
+  uploadPreset: 'paup0jwu'}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+      console.log('Done! Here is the image info: ', result.info); 
+      url = result.info.url;
+      const image = document.createElement("img");
+      image.setAttribute("src", url);
+      const songPhoto = document.querySelector(".song-photo");
+      songPhoto.append(image);
+    }
+  }
+)
+
+document.getElementById("upload_widget").addEventListener("click", function(){
+    myWidget.open();
+  }, false);
+  document.querySelector('.song-form').addEventListener('submit', post);
+   
